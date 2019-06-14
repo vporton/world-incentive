0a418e541e4f884c679504575558ef83b0639f37
Victor Porton <porton@narod.ru>
2019-06-14 18:14:26 +0300

    initiative models

--- incentive/models.py
+++ incentive/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.translation import gettext as _
 
 
 class Country(models.Model):
@@ -14,5 +15,41 @@ class City(models.Model):
     ]
 
 
-# class Incentive(models.Model):
-#     pass
+class InitiativeCategory(models.Model):
+    name = models.CharField(max_length=255)
+
+
+class InitiativeImage(models.Model):
+    initiative = models.ForeignKey('Initiative', on_delete=models.CASCADE)
+    image = models.FileField(_("Attached image"))
+
+
+class InitiativeFile(models.Model):
+    initiative = models.ForeignKey('Initiative', on_delete=models.CASCADE)
+    file = models.FileField(_("Attached file"))
+
+
+class Initiative(models.Model):
+    LEVEL_WORLD = 0
+    # LEVEL_UNION = 1
+    LEVEL_COUNTRY = 2
+    # LEVEL_REGION = 3
+    LEVEL_CITY = 4
+    # LEVEL_MUNICIPAL = 5
+
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    level = models.SmallIntegerField(_("Initiative level"))
+    country = models.ForeignKey(Country, on_delete=models.SET_NULL, null=True)
+    city = models.ForeignKey(City, on_delete=models.SET_NULL, null=True)
+
+    title = models.CharField(max_length=255)
+    problem = models.TextField()
+    solution = models.TextField()
+    outcome = models.TextField()
+
+    categories = models.ManyToManyField(InitiativeCategory)
+
+    votes_for = models.BigIntegerField(_("Votes for"))
+    votes_against = models.BigIntegerField(_("Votes against"))
