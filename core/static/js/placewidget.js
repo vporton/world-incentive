52cf2155de37cc95a4d1d7ffd376cd061ce72c33
Victor Porton <porton@narod.ru>
2019-06-17 00:27:20 +0300

    bug fix

--- core/static/js/placewidget.js
+++ core/static/js/placewidget.js
@@ -1,7 +1,7 @@
 // TODO: No fixed ID.
 function update_places_list(level, from_initial=false) {
     level = Number(level);
-    const val = $('#id_place_' + (level - 1)).val();
+    const val = $(from_initial ? '#id_place_' + (level - 1) + '_initial' : '#id_place_' + (level - 1)).val();
     if(val == '')
         for(var cur = level; cur < 5; ++cur)
             $('#id_place_'+cur).html("<option value=''>-</option>");
@@ -10,6 +10,6 @@ function update_places_list(level, from_initial=false) {
             $('#id_place_'+level).html("<option value=''>-</option>\n" + data);
             if(from_initial) $('#id_place_'+level).val($('#id_place_'+level+'_initial').val());
             if(level < 4)
-                update_places_list(level + 1);
+                update_places_list(level + 1, from_initial);
         });
 }
