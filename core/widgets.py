40170bbfad3dee196c36f0a184f897ecc5dc3f60
Victor Porton <porton@narod.ru>
2019-06-19 17:10:01 +0300

    bug fix

--- core/widgets.py
+++ core/widgets.py
@@ -1,6 +1,7 @@
 from cities.models import Country
 from django import forms
 from django.forms import widgets
+from django.utils.functional import cached_property
 from languages.forms import LanguageField
 
 
@@ -11,7 +12,11 @@ class PlaceWidget(widgets.MultiWidget):
         js = ('js/placewidget.js',)
 
     def __init__(self, attrs=None):
-        _widgets = (
+        super().__init__([], attrs)
+
+    @cached_property
+    def widgets(self):
+        return (
             widgets.Select(choices=[('', '-')] + [(c.pk, c.name) for c in Country.objects.all().order_by('name')],
                            attrs={'onchange': "update_places_list(1)", **(attrs or {})}),
             widgets.Select(choices=[('', '-')],
@@ -22,7 +27,6 @@ class PlaceWidget(widgets.MultiWidget):
                            attrs={'onchange': "update_places_list(4)", **(attrs or {})}),
             widgets.Select(choices=[('', '-')])
         )
-        super().__init__(_widgets, attrs)
 
     def decompress(self, value):
         if value:
