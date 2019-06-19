c6fe5107f92243d0b45aa4e0bed3ca9a37663fbd
Victor Porton <porton@narod.ru>
2019-06-19 21:02:58 +0300

    showing initiative

--- initiative/urls.py
+++ initiative/urls.py
@@ -4,6 +4,7 @@ from initiative import views
 
 app_name = 'initiative'
 urlpatterns = [
+    path('view/<int:initiative_pk>', views.ShowInitiativeView.as_view(), name='view'),
     path('create-prompt', views.CreateInitiativePromptView.as_view(), name='create-prompt'),
     path('create', views.CreateInitiativeView.as_view(), name='create'),
 ]
--- initiative/views.py
+++ initiative/views.py
@@ -1,11 +1,38 @@
+import bleach
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.http import HttpResponse
 from django.shortcuts import render
+from django.utils.safestring import mark_safe
 from django.utils.translation import get_language_from_request
 from django.views import View
 from django.utils.translation import gettext as _
 
 from initiative.forms import CreateInitiativePromptForm, InitiativeForm
+from initiative.models import InitiativeLanguage
+
+
+class ShowInitiativeView(View):
+    def get(self, request, initiative_pk):
+        lang = request.GET.get('lang', '')
+        language_codes = lang.split(',')
+
+        lang_obj = None
+        for language_code in language_codes:
+            try:
+                lang_obj = InitiativeLanguage.objects.get(initiative__pk=initiative_pk, language=language_code)
+            except InitiativeLanguage.DoesNotExist:
+                pass
+            else:
+                break
+
+        version = lang_obj.last_version if lang_obj else None
+
+        problem = version and mark_safe(bleach.clean(version.problem, tags=bleach.sanitizer.ALLOWED_TAGS + ['p']))
+        solution = version and mark_safe(bleach.clean(version.solution, tags=bleach.sanitizer.ALLOWED_TAGS + ['p']))
+        outcome = version and mark_safe(bleach.clean(version.outcome, tags=bleach.sanitizer.ALLOWED_TAGS + ['p']))
+        return render(request, 'initiative/view.html', {'version': version,
+                                                        'problem': problem,
+                                                        'solution': solution,
+                                                        'outcome': outcome})
 
 
 class CreateInitiativePromptView(LoginRequiredMixin, View):
