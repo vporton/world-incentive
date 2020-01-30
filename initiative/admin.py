0c367ad97755b33de2d8a2364b161830a19e1ee3
Victor Porton <porton@narod.ru>
2020-01-30 11:39:14 +0200

    bug

--- initiative/admin.py
+++ initiative/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from django.apps import apps
-
-
-models = apps.get_models()
-
-for model in models:
-    admin.site.register(model)
+# from django.apps import apps
+#
+#
+# models = apps.get_models()
+#
+# for model in models:
+#     admin.site.register(model)
