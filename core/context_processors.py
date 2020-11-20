d12b6fabeec40a7394ee52fa6d8cfe3ad147aaf9
Victor Porton <porton@narod.ru>
2020-11-20 02:35:44 +0200

    bug fix

--- core/context_processors.py
+++ core/context_processors.py
@@ -1,4 +1,5 @@
 from django.conf import settings
+from django.middleware.csrf import get_token
 from django.utils.translation import get_language_from_request
 
 from core.forms import LanguageChoiceForm
@@ -15,6 +16,7 @@ def language_choice_form_context_processor(request):
     language = get_language_from_request(request, check_path=True)
     user_languages = [language] if request.user.is_anonymous \
         else request.user.languages.all().values_list('language', flat=True)
+    get_token(request)  # to turn on CSRF
     return {
         'lang': language,
         'user_languages': ','.join(user_languages),
